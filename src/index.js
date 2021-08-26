a3a49f7dc587607ef94fe38fe20aa5891567d93b
developer-pritam <developerpritam@gmail.com>
2021-08-26 18:23:22 +0530

    Eco Link User V1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
+import Main from './Main'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" >
+          {() => {
+            window.open("https://google.com", '_top')
+            return (
+              <></>
+            )
+          }}
+        </Route>
+        <Route path="/:keyID" >
+          <Main />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/test.js
@@ -0,0 +1,14 @@
+let oj = {
+    link: "developerpritam.in",
+    count: 45,
+    Browser: {
+        Chrome: 45,
+    }
+}
+
+console.log(oj.Browser.gu)
+
+console.log({
+    count: oj.Browse ? oj.Browser : 45,
+
+})
